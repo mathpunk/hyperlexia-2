4246d0b0b01fb595e864a02642f90cd0105555df
thomas henderson <tom@mathpunk.net>
2016-10-13 15:47:07 -0700

    Trying, failing, to include a file's data

--- src/cljs/anansi/reading/app.cljs
+++ src/cljs/anansi/reading/app.cljs
@@ -1,13 +1,13 @@
 (ns anansi.reading.app
-  (:require [reagent.core :as reagent :refer [atom]]))
+  (:require [reagent.core :as reagent :refer [atom]]
+            [anansi.reading.db :as db]))
 
 (defn some-component []
   [:div
    [:h3 "I am a component!"]
    [:p.someclass
-    "I have " [:strong "bold"]
-    [:span {:style {:color "green"}} " and green"]
-    " text."]])
+    "I represent " [:strong db/c]
+    [:span {:style {:color "green"}} " elements"]]])
 
 (defn calling-component []
   [:div "Parent component"
--- src/cljs/anansi/reading/db.cljs
+++ src/cljs/anansi/reading/db.cljs
@@ -1,2 +1,10 @@
 (ns anansi.reading.db
   (:require [cognitect.transit :as transit]))
+
+(slurp "resources/recent_pins.json")
+
+(def pin-data (.fileRead fs "data/recent_pins.json"))
+
+(def r (transit/reader :json))
+
+(def c (count (r pin-data)))
