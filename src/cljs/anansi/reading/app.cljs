db823604a93864336407a2c631e6052d4d98c3ec
thomas henderson <tom@mathpunk.net>
2016-10-13 20:25:02 -0700

    chasing a weird bug around js->cljs maybe?

--- src/cljs/anansi/reading/app.cljs
+++ src/cljs/anansi/reading/app.cljs
@@ -1,32 +1,46 @@
 (ns anansi.reading.app
   (:require [reagent.core :as reagent :refer [atom]]
-            [anansi.reading.db :as db]))
+            [anansi.reading.db :as db] ))
 
 (def state
   (let [data db/data
-        view {:posts {:active (nth (:posts data) 0)}}]
+        view {:view {:posts {:active (nth (:posts data) 0)}}}]
     (atom (merge data view))))
 
-(defn card [{:keys []}]
-  "Our first real component."
-  []
-  )
+; (defn card [{:keys [href tags], :as props}]
+;   "Our first real component."
+;   (do (. js/console log href)
+;     ;; [:div {:style {:background-color "light blue"}} [:span.href {:style {:color "violet"}} href]]
+;     [:div {:style {:background-color "blue" :height 120 :width 450}}] ))
+
+; (defn card [{:keys [href description extended meta hash time shared toread tags] :as props}]
+;   [:p "decsription"])
+
+; (defn card [c]
+;   [:p (js/JSON.stringify c)])
+
 
 (defn summary [data]
-  (do (. js/console log "input: " (js/JSON.stringify db/input))
-   (. js/console log "data: " @state)
     [:div#summary
-      [:div#today "today's date is 2016-10-13"]
-      [:div#recent "these posts are from " (:date @state)]
+      [:div#today "today is 2016-10-13"]
+      [:div#source (str "viewing " (:user @state) "'s pins, retrieved " (:date @state) )]
       [:div#data
         [:div#keys "They have these keys: "
           (clojure.string/join  " " (:keys @state))]
         [:div#user "They have this user: " (:user @state)]
         [:div#posts "There are fewer than " (+ 1 (count (:posts @state))) " posts"
-        [:div#active-post "This is the first post, unrendered: " (first (:posts @state))]
-        [:div.post [:p "The keys of a post are"  (get-in @state [:view :active])]]
-        ]
-      ]]))
+        [:div#post-data "The keys of a post are "
+          (let [post ;(get-in @state [:view :posts :active])]
+                 {:hi "there"}]
+            [:p (keys post)])
+            [:p "What we care about are firstly, "]
+            [:code "href tags"]
+            [:p "What we care about are secondly, "]
+            [:code "href description extended tags"]
+            [:article#components
+              [:h2 "part 2: real components"]
+              ;; [card (first (:posts @state))]
+            ] ]]]])
 
 (defn welcome []
   [:div#welcome "Good morning--"])
