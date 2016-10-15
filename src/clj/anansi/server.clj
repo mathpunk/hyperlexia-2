6b0668e60b22f0e085d025267d599add06719360
thomas henderson <tom@mathpunk.net>
2016-10-15 14:31:11 -0700

    f it, ignore static and work on the server

--- src/clj/anansi/server.clj
+++ src/clj/anansi/server.clj
@@ -1,9 +1,22 @@
 (ns anansi.server
   (:require [compojure.core :refer :all]
             [ring.middleware.resource :refer [wrap-resource]]
+            [ring.middleware.content-type :refer [wrap-content-type]]
+            [ring.middleware.not-modified :refer [wrap-not-modified]]
             [compojure.route :as route]))
 
-(defroutes server
-  (route/resources "/")
-  (GET "/hello" [] "<h1>Hello World</h1>")
+(defroutes app
+  ;; (route/resources "/")
+  (GET "/data" []
+    (do (println "data")
+         "<h1>Data route</h1>"))
   (route/not-found "<h1>Page not found</h1>"))
+
+
+
+
+(def server
+  (-> app
+      (wrap-resource "public")
+      (wrap-content-type)
+      (wrap-not-modified)))
